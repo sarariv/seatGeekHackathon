fb78a4955eca6967544b2fb5242483d3c049e5c1
Jillian Campbell <jill.campbell07@gmail.com>
2016-10-15 18:48:42 -0400

    Added accessible list option

--- app/assets/javascripts/application.js
+++
@@ -1,7 +0,0 @@
- $.ajax({
-                  url: '/venues',
-                  dataType: 'json',
-                  data: [events{response:response, city:city, state:state, "authenticity_token": AUTH_TOKEN}
-                })
-              })
-            });
