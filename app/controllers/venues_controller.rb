f199c692cc5e248c657be3392392fbdea72cb42b
Saralis Rivera <saralis.rivera@gmail.com>
2016-10-15 16:18:33 -0400

    add search in venue index

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,18 +1,10 @@
 class VenuesController < ApplicationController
 
 	def index
-    @search_terms= params[:search_terms]
+    @search = params[:search]
+
+    @search_results = HTTParty.get("https://api.seatgeek.com/2/events?q=#{@search}")
+
 	end
-
-  def search
-    search_params = { search_terms: params[:search_terms], limit: 10 }
-
-    begin
-      results = "https://api.seatgeek.com/2/events?q=#{search_params}"
-    rescue
-      return render json: []
-    end
-  end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
 
-  resources :venues, only: [:index, :show] do
-    get 'search'
-  end
+  resources :venues, only: [:index, :show]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
