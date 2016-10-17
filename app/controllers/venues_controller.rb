56db6e41046a0d733b12a3190b4ef567345032f1
Saralis Rivera <saralis.rivera@gmail.com>
2016-10-16 23:51:07 -0400

    correct display results when searching by zipcode

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,10 +3,13 @@ class VenuesController < ApplicationController
 	def index
     @search = params[:search]
 
+
     @search_result = HTTParty.get("https://api.seatgeek.com/2/venues/?q=#{@search}")
+
 
     @location = @search_result["venues"].select do |result|
       Venue.find_by(name: result["name"])
+
     end
 
     @venue_match = @location.map do |match|
@@ -17,6 +20,9 @@ class VenuesController < ApplicationController
 
   def show
     @venue = Venue.find_by(id: params[:id])
+
+    @sgdata = HTTParty.get("https://api.seatgeek.com/2/venues/#{@venue.sgID}")
+
   end
 
 end
